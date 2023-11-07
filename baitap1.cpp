2e02ca2e37f5b8aac60dcda53bc7fd32f3b678a5
hunganh010805 <147390474+hunganh010805@users.noreply.github.com>
2023-11-07 16:01:32 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+	int canh_dai_hcn=8;
+	int canh_ngan_hcn=5;
+	int chu_vi_hcn=((canh_dai_hcn+canh_ngan_hcn)*2);
+	int canh_hv=4;
+	int chu_vi_hv=(canh_hv*4);
+	int dien_tich_hv=(canh_hv*canh_hv);
+	float PI=3.14;
+	int ban_kinh = 7;
+	float chu_vi_htr=(PI*ban_kinh*2);
+	float dien_tich_htr=((PI*ban_kinh)*2);
+	printf("chu_vi_htr=%f",chu_vi_htr);
+	}
+	
+	
+	
